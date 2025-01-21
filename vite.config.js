70915ec5a3f5f31a050a6e0751455faf516bf274
Thanveer Sherief - vn55jeb <thanveer.sherief@walmart.com>
2025-01-21 19:39:31 +0530

    test2  commit

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,6 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     lib: {
-//       entry: './src/main.jsx', 
-//       name: 'HelloWorldApp',
-//       fileName: (format) => `hello-world-app.${format}.js`,
-//     },
-//     rollupOptions: {
-//       external: ['react', 'react-dom'],
-//       output: {
-//         globals: {
-//           react: 'React',
-//           'react-dom': 'ReactDOM',
-//         }
-//       },
-//     },
-//   },
-// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Export the Vite config
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -33,18 +10,41 @@ export default defineConfig({
       fileName: (format) => `hello-world-app.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'], // Externalize react and react-dom
+      external: ['react', 'react-dom'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
-        },
+        }
       },
     },
-    outDir: 'dist',  // Output directory
-    assetsDir: 'assets', // Directory for assets like images
   },
 });
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // Export the Vite config
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     lib: {
+//       entry: './src/main.jsx', 
+//       name: 'HelloWorldApp',
+//       fileName: (format) => `hello-world-app.${format}.js`,
+//     },
+//     rollupOptions: {
+//       external: ['react', 'react-dom'], // Externalize react and react-dom
+//       output: {
+//         globals: {
+//           react: 'React',
+//           'react-dom': 'ReactDOM',
+//         },
+//       },
+//     },
+//     outDir: 'dist',  // Output directory
+//     assetsDir: 'assets', // Directory for assets like images
+//   },
+// });
 
 
 
